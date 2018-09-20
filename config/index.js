7354f45665bf32db92b7f408029d1f07ffb3272b
pengyang <pengyang@huitongjy.com>
2018-09-20 15:53:12 +0800

    first

--- config/index.js
+++ config/index.js
@@ -4,8 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../myapp/dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../myapp/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
